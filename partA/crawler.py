4ac0a0f7f19116ce7095a331674a9a09be1f699c
Srikanth Reddy Malipatel <srikk595@gmail.com>
2015-09-10 13:24:23 -0400

    writing a list of json objects to a file

--- partA/crawler.py
+++ partA/crawler.py
@@ -13,25 +13,48 @@
 from twython import Twython
 from twython import TwythonStreamer
 import json, io
+import sys
 
 consumer_key = "Fpsk6Yi6owCPtLjSXaUoZEK7c"
 consumer_secret_key = "YezwQhE3Kh3dgXGNVGXUOYhXmTkE38QgmbpToXTc29DZWEnNuF"
 access_token = "140017490-REcjx2p6YphicskjJkl61PrHP5hNsYCgdVOoOflP"
 access_secret_token = "lHrn7XJiwlnggxFuahDdDkphewnWmIXuvIpE6dAz1NMhj"
-tweet_count=0
 
+# Keywords
+keywords_en = "presidential campaign, #POTUS, "
+keywords_ru = "Коммунистическая партия Советского Союза, Дума, Единая Россия, Либерально-демократическая партия России, Владимир Вольфович Жириновский, национализм и популизм, социального консерватизма, Справедливая Россия, социал-демократия, новости, Путин"
+keywords_de = ""
+
+# Global Variables
+tweet_count_en = 200
+tweet_count_ru = 0
+tweet_count_de = 200
+
+tweet_list = []
+
+# todo: add all tweets to a single json list
 
 class MyStreamer(TwythonStreamer):
     def on_success(self, data):
+        global tweet_count_ru
+        global tweet_list
         if 'text' in data:
-            with io.open('tweet_data.txt', 'w', encoding='utf-8') as f:
-                print data
-                f.write(unicode(json.dumps(data, ensure_ascii=False)))
+            # append incoming tweets to a list
+            tweet_list.append(data)
+            tweet_count_ru += 1
+            print tweet_count_ru
+
+            # when you reach certian number of tweets then just dump the list into a file
+            if tweet_count_ru == 3:
+                with io.open('tweet_data.txt', 'a', encoding='utf-8') as f:
+                    f.write(unicode(json.dumps(tweet_list, ensure_ascii=False)))
+                sys.exit()
 
 
     def on_error(self, status_code, data):
         print status_code
 
 if __name__ == '__main__':
+    
     stream = MyStreamer(consumer_key, consumer_secret_key, access_token, access_secret_token)
-    stream.statuses.filter(track="lol, modi, bernie", language="en")
+    stream.statuses.filter(track=keywords_ru, language="ru")
